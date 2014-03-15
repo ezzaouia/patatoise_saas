66b50e7b328fb39f8f2d6f57ac39b3f8c42c5391
Ezzaouia <mohamed.ezzaouia@um5s.net.ma>
2014-03-15 11:00:54 +0000

    first feature Sign Up implemented

---
+++ spec/dummy/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Dummy::Application.config.secret_key_base = '35a02bb6cf9caefa2f5f54e21bd8445e81ed7e1233c4968b24af81ff02eb9398de4486cbb7455c30c499751872310ab2c12644e1e98f8298a68349f175f913c6'
